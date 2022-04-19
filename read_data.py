cdb3481e7dd9d2c02d10b0b813a61e49c6e31b2d
Ahmad <huntertom417@gmail.com>
2022-04-19 10:37:07 +0500

    read_file returns record count

--- read_data.py
+++ read_data.py
@@ -5,8 +5,8 @@ def read_file(filename):
     data:List[EmployeeRecord] = []
     with open(filename,"r") as file:
         file.readline()
-        file.readline()
+        record_count = int(file.readline())
         for record in file.readlines():
             rec = EmployeeRecord(*record.split("\t"))
             data.append(rec)
-    return data
+    return (data,record_count)
