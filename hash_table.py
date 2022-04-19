999cfb4fde8c9a79dc05b01f4c92d2a9dfc7cccb
Ahmad <huntertom417@gmail.com>
2022-04-19 10:46:36 +0500

    Hash takes only int ID instead of entire record

--- hash_table.py
+++ hash_table.py
@@ -6,14 +6,14 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.len = 0
-        self.data:List[EmployeeRecord] = [None]*(size+size*0.4) #40 prcnt more space allocated
+        self.data:List[EmployeeRecord] = [None]*(int(size+size*0.4)) #40 prcnt more space allocated
 
     def insert(self, record: EmployeeRecord):
-        slot = self.find_empty(record)
+        slot = self.find_empty(record.ID)
         self.data[slot] = record
     
-    def get(self, record: EmployeeRecord):
-        slot = self.find_record(record)
+    def get(self, recordId):
+        slot = self.find_record(recordId)
         if slot is not None:
             return self.data[slot]
         else:
@@ -25,13 +25,13 @@ class HashTable:
             self.data[slot] = None
 
 
-    def hash(self, record:EmployeeRecord):
-        return record.ID%self.size
+    def hash(self, recordId):
+        return recordId%self.size
     
-    def probe(self,record:EmployeeRecord,i):
-        return (self.hash(record)+i**2)%self.size
+    def probe(self,recordId,i):
+        return (self.hash(recordId)+i**2)%self.size
     
-    def find_empty(self,key:EmployeeRecord):
+    def find_empty(self,key):
         i = self.hash(key)
         j = 1
         while (self.data[i]!=None) and j<=self.size:
@@ -43,7 +43,7 @@ class HashTable:
         i = self.hash(key)
         j = 1
         while j<=self.size and self.data[i]!=None:
-            if self.data[i].ID == key.ID:
+            if self.data[i].ID == key:
                 break
             i = self.probe(key,j)
             j+=1
