6ba0ccd9a58669c619cc75682d30072548190f21
Ahmad <huntertom417@gmail.com>
2022-04-22 14:45:13 +0500

    Added functionality to get size of Employee record object

--- record.py
+++ record.py
@@ -1,6 +1,9 @@
+from sys import getsizeof
 class EmployeeRecord:
-    def __init__(self,ID,name,city,cat) -> None:
+    def __init__(self,ID,name,city,cat):
         self.ID = int(ID)
         self.name = name
         self.city = city
         self.cat = cat
+    def __sizeof__(self):
+        return getsizeof(self.ID)+ getsizeof(self.name) + getsizeof(self.city) + getsizeof(self.cat)
