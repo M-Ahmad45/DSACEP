961ce83d81261147286d690b40f672df84414b6d
Ahmad <huntertom417@gmail.com>
2022-04-22 14:33:04 +0500

    added functions for insertion sorting printing and timing

--- utilis.py
+++ utilis.py
@@ -1,5 +1,7 @@
 from record import EmployeeRecord
 from typing import List
+from time import perf_counter
+from hash_table import HashTable
 
 def read_file(filename):
     data:List[EmployeeRecord] = []
@@ -10,3 +12,33 @@ def read_file(filename):
             rec = EmployeeRecord(*record.split("\t"))
             data.append(rec)
     return (data,record_count)
+
+def timer(func):
+    def inner(*args):
+        t1 = perf_counter()
+        func(*args)
+        t2 = perf_counter()
+        return t2-t1
+    return inner
+@timer
+def insert(table:HashTable, data):
+    for i in data:
+        table.insert(i)
+
+def sort_data(data:List[EmployeeRecord]):
+    data.sort(key= lambda x:x.ID)
+
+@timer
+def sort_traverse(table:HashTable,data:List[EmployeeRecord],print_on=True):
+    sort_data(data)
+    for i in data:
+        d = table.get(i.ID)
+        if print_on:
+            print(d)
+@timer
+def find(table:HashTable,data,print_on=True):
+    for i in range(len(data)):
+        if i%2==0:
+            d = table.get(i)
+            if print_on:
+                print(d)
